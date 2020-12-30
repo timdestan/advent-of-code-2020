54e4f371fef43a75edeb40d29a65beb9d5339cfd
Tim Destan <tim.destan@gmail.com>
2020-12-30 13:22:17 -0500

    Try to make the Ruby version as fast as possible.
    `loop do` and globals are suprisingly expensive and were accounting
    for most of the time.

--- src/bin/day23.rb
+++ src/bin/day23.rb
@@ -2,68 +2,51 @@
 
 require_relative '../lib.rb'
 
-$nmoves = 10_000_000
+nmoves = 10_000_000
 
 cups = IO.read("data/day23input.txt").chomp.split('').map(&:to_i)
 cups += (cups.max + 1 .. 1_000_000).to_a
-$ncups = cups.size
+ncups = cups.size
 
 prev = nil
-$next = []
+next_cup = []
 cups.each do |c|
   unless prev.nil?
-    $next[prev] = c
+    next_cup[prev] = c
   end
   prev = c
 end
-$next[prev] = cups[0]
+next_cup[prev] = cups[0]
 
 curr = cups[0]
 cups = nil
 
-def find_dest(curr, x, y, z)
+move = 1
+while move <= nmoves
+  # Find cups to move (x, y, z).
+  x = next_cup[curr]
+  y = next_cup[x]
+  z = next_cup[y]
   i = curr - 2
-  loop do
-    curr = i % $ncups + 1
-    unless curr == x || curr == y || curr == z
-      return curr
+  while true
+    dest = i % ncups + 1
+    unless dest == x || dest == y || dest == z
+      break
     end
     i -= 1
   end
+
+  tmp = next_cup[dest]
+  next_cup[curr] = next_cup[z]
+  next_cup[dest] = x
+  next_cup[x] = y
+  next_cup[y] = z
+  next_cup[z] = tmp
+
+  curr = next_cup[curr]
+  move += 1
 end
 
-def find_to_move(curr)
-  arr = []
-  3.times do
-    arr << $next[curr]
-    curr = $next[curr]
-  end
-  arr
-end
-
-$move = 1
-$nmoves.times do
-  # puts "-- move #{$move} --"
-  # puts "cups: #{$cups.map.with_index {
-  #   |c,i| i == curr ? "(#{c})" : c.to_s
-  # }.join(' ')}"
-  # puts "next = #{$next.inspect}"
-  x, y, z = find_to_move(curr)
-  # puts "pick up: #{to_move.map(&:to_s).join(', ')}"
-  dest = find_dest(curr, x, y, z)
-  # puts "destination: #{dest}"
-
-  tmp = $next[dest]
-  $next[curr] = $next[z]
-  $next[dest] = x
-  $next[x] = y
-  $next[y] = z
-  $next[z] = tmp
-
-  curr = $next[curr]
-  $move += 1
-end
-
-x = $next[1]
-y = $next[x]
+x = next_cup[1]
+y = next_cup[x]
 puts "order = #{x} * #{y} = #{x * y}"
